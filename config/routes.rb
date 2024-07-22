8322d7ed516c6b43b97c45e114a607e487fc51d5
Noriko Iida <lumos.5503@gmail.com>
2024-07-22 14:34:21 +0000

    7/22

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-　devis_for :users
   root to: "homes#top"
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+  }
+  get "homes/about" => "homes#about", as: "about"
 
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy ]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
