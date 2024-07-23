90c596776395de28eae781c2d6d6ec8a56a68ecc
Noriko Iida <lumos.5503@gmail.com>
2024-07-23 14:31:07 +0000

    7/23

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,12 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
 
   protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
   def index
-
+    
   end
 
   def show
-  end
-
-  def edit
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 end
