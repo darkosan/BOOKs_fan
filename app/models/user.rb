5aebf4a5f5f099bff52a937b1f519fa4b7dd3d6b
Noriko Iida <lumos.5503@gmail.com>
2024-09-04 12:08:29 +0000

    3rdデプロイ+テスト完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable # :recoverable, :rememberable
+  devise :database_authenticatable, :registerable, :validatable
+       # :recoverable, :rememberable
 
   has_many :books, dependent: :destroy
   has_many :post_comments, dependent: :destroy
