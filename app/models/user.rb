4b0ae2be7db4dcf6d79531edeef85d5080a97e2d
darkosan <lumos.5503@gmail.com>
2024-11-10 09:46:07 +0000

    11/10

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,8 @@ class User < ApplicationRecord
 
   def get_profile_image
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_user.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join("app/assets/images/no_user.jpg")
+      profile_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
     end
     profile_image.variant(resize_to_limit: [100, 100]).processed
   end
